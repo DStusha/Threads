7661b2cb18b7c3807f4b799db54b8e609dce1a12
DStusha <degtiarewa.nastya@yandex.ru>
2018-08-06 17:14:55 +0300

    second

--- Threads/MyThread.cs
+++ Threads/MyThread.cs
@@ -16,11 +16,14 @@ namespace Threads
 
         void func()
         {
-            while (ClassFunc.InterlockedExchange(ref Program.flag, 1) == 1) Thread.Sleep(0);
-            Console.WriteLine("Start" +" "+ Thread.CurrentThread.Name);
-            Thread.Sleep(100);
-            Console.WriteLine("Stop" + " " + Thread.CurrentThread.Name);
-            Program.flag = 0;
+            while (true)
+            {
+                while (ClassFunc.InterlockedExchange(ref Program.flag, 1) == 1) Thread.Sleep(0);
+                Console.WriteLine("Start" + " " + Thread.CurrentThread.Name);
+                Thread.Sleep(100);
+                Console.WriteLine("Stop" + " " + Thread.CurrentThread.Name);
+                Program.flag = 0;
+            }
         }
     }
 }
